87d297f082eedeaede88a69b098fd69271fd82e2
Salman <badshah7098891@gmail.com>
2026-04-14 00:17:30 +0500

    Add initial screen components for Analytics, Home, and Settings

---
+++ src/screens/analyticsScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AnalyticsScreen() {
+  return (
+    <View>
+      <Text>analyticsScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ src/screens/settingsScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function SettingsScreen() {
+  return (
+    <View>
+      <Text>settingsScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
